f894671b54eae95280858fa4d89220b2a840d125
windmeup <babamiya224@gmail.com>
2023-09-02 16:18:07 +0800

    format with gdformat

--- 2d/dodge_the_creeps/Player.gd
+++ 2d/dodge_the_creeps/Player.gd
@@ -2,8 +2,10 @@ extends Area2D
 
 signal hit
 
-@export var speed = 400 # How fast the player will move (pixels/sec).
-var screen_size # Size of the game window.
+@export var speed = 400  # How fast the player will move (pixels/sec).
+
+var screen_size  # Size of the game window.
+
 
 func _ready():
 	screen_size = get_viewport_rect().size
@@ -11,7 +13,7 @@ func _ready():
 
 
 func _process(delta):
-	var velocity = Vector2.ZERO # The player's movement vector.
+	var velocity = Vector2.ZERO  # The player's movement vector.
 	if Input.is_action_pressed(&"move_right"):
 		velocity.x += 1
 	if Input.is_action_pressed(&"move_left"):
@@ -46,7 +48,7 @@ func start(pos):
 
 
 func _on_Player_body_entered(_body):
-	hide() # Player disappears after being hit.
+	hide()  # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	$CollisionShape2D.set_deferred(&"disabled", true)
